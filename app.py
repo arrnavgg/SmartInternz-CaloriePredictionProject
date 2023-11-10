44c4de9bfe535107f04e25cd791e5d009022b5a2
Vansh Raja <64516886+UrAvgProGamer@users.noreply.github.com>
2023-11-10 18:04:01 +0530

    basic functioning register page

--- app.py
+++ app.py
@@ -6,90 +6,67 @@ app = Flask(__name__)
 app.config['DATABASE'] = 'Calorie.db'
 app.secret_key = 'TH15_1S_@_S3CR3T_K3Y'
 
-
 @app.route('/login',methods=("GET","POST"))
 def login():
-    if request.method=="POST":
-        login_id=request.form['userid']
-        passwd = request.form['password']
-        login_conn = sqlite3.connect(app.config['DATABASE'])
-        cur=login_conn.cursor()
-        cur.execute('SELECT password,code FROM users WHERE username = ? password = ?', (login_id,passwd))
-        result = cur.fetchone()
-        login_conn.close()
-        if result:
-            print(result)
-            session['userid'] = request.form['userid']
-            session['name'] = request.form['name']
-            session['age'] = request.form['age']
-            session['height'] = request.form['height']
-            session['gender'] = request.form['gender']
-            return redirect(url_for('home'))
-        else:
-            print("Wrong")
-            return render_template('login.html',message="Wrong Id or Password")
+    pass
 
+from flask import Flask, render_template, request, redirect, url_for, flash
+import sqlite3
+
+app = Flask(__name__)
+app.config['DATABASE'] = 'Calorie.db'
+app.secret_key = 'TH15_1S_@_S3CR3T_K3Y'
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+
+        # Get user input from the registration form
+        userid = request.form['username']
+        password = request.form['password']
+        name = request.form['name']
+
+        # Connect to the SQLite database
+        with sqlite3.connect(app.config['DATABASE']) as conn:
+            cursor = conn.cursor()
+
+            # Check if the user already exists
+            cursor.execute("SELECT userid FROM users WHERE userid=?", (userid,))
+            existing_user = cursor.fetchone()
+
+            if existing_user:
+                # User already exists, render the registration form with an error message
+                flash("User already exists. Please choose a different username.")
+                return render_template('register.html')
+
+            # Insert the new user
+            cursor.execute("INSERT INTO users (userid, password, name) VALUES (?, ?, ?)",
+                           (userid, password, name))
+
+        flash("Registration successful. You can now log in.")
+        return redirect(url_for('login'))  # Redirect to the login page after registration
+
+    return render_template('register.html')  # Render the registration form template
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
     return render_template('login.html')
 
-@app.route('/register.html',methods=("GET","POST"))
-def register():
-    if request.method=="POST":
-        user_id=request.form['userid']
-        passwd = request.form['password']
-        name = request.form['name']
-        age = request.form['age']
-        height = request.form['height']
-        gender = request.form['gender']
-
-        register_conn = sqlite3.connect(app.config['DATABASE'])
-        register_cur=register_conn.cursor()
-        register_cur.execute('SELECT userid FROM users WHERE userid = ?', (user_id,))
-        result = register_cur.fetchone()
-        if result:
-            return render_template('register.html',message="User already exists")
-        else:
-            register_cur.execute('insert into users values(?,?,?,?,?)',(user_id,passwd,name,age,height,gender))
-            register_conn.commit()
-            session['userid'] = request.form['userid']
-            session['name'] = request.form['name']
-            session['age'] = request.form['age']
-            session['height'] = request.form['height']
-            session['gender'] = request.form['gender']
-            return redirect(url_for('home'))
-        
-@app.route('/home.html',methods=("GET","POST"))
-def home():
-    if request.method=="POST":
-        gender_text=request.form['gender']
-        Gender = 0 if gender_text=='Male' else 1
-        Age = float(request.form['age'])
-        Height = float(request.form['height'])
-        Duration=float(request.form['duration'])
-        Heart_Rate = float(request.form['heart_rate'])
-        Body_Temp = float(request.form['temperature'])
-        date=os.date.today()
-        model=load_model('model.h5')
-        result = model.predict([[Gender,Age,Height,Duration,Heart_Rate,Body_Temp]])
-        session['calories'] = result
-        exercise_conn = sqlite3.connect(app.config['DATABASE'])
-        exercise_cur=exercise_conn.cursor()
-        exercise_cur.execute('insert into exercise(exercise_name,userid,duration,date,bpm,temperature,calories) values(?,?,?,?,?,?,?)',(request.form['exercise'],session['userid'],Duration,date,Heart_Rate,Body_Temp,result))
-        return render_template('home.html',calories = result)
-        
-
-    return render_template('home.html',name = session['name'], age = session['age'], height = session['height'], gender = session['gender'])
-
-@app.route('/dashboard.html',methods=("GET","POST"))
+@app.route('/dashboard',methods=("GET","POST"))
 def dashboard():
     pass
 
+@app.route('/home',methods=("GET","POST"))
+def home():
+    pass
 
-# Afterwards can just redirect it to the login page directly, or can display a
+
+# Below we can just redirect it to the login page directly, or can display a
 # Homepage with dev info/project info and login button.
 
 @app.route('/')
-def method_name():
-    return "HELLO WORLD!"
+def index():
+    return render_template('base.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
